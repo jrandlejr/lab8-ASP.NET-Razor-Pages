cf86bf83d8c2a061358fe511af62c580620f62b4
jrandlejr <randleja@hendrix.edu>
2026-03-10 17:57:33 -0500

    Installing more packages

--- MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
+++ MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d306863ad03fabf9def5542adf76c21e204eaa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12b8c18cf3c51e7a7283cdc67b55730a7770e8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
