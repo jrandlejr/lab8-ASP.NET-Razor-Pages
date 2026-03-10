6d306863ad03fabf9def5542adf76c21e204eaa3
jrandlejr <randleja@hendrix.edu>
2026-03-10 17:51:34 -0500

    Pudotnet add package Microsoft.EntityFrameworkCore.Sqlite

--- MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
+++ MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4d57aa0bdb372fc969f5728861433715d66015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a77682ecc056d0bf8610f38f3ee9eee5a7888f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
