10e6bd9806efd72cf7ca893079db1f3c5e863946
jrandlejr <randleja@hendrix.edu>
2026-03-10 19:10:31 -0500

    Add validation to Album model same from tutorial

--- MusicAlbums/Models/Album.cs
+++ MusicAlbums/Models/Album.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MusicAlbums.Models;
 
 public class Album
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
     public string Artist { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1900, 2026)]
     public int Year { get; set; }
+
+    [Required]
+    [StringLength(30)]
     public string Genre { get; set; } = string.Empty;
 }
