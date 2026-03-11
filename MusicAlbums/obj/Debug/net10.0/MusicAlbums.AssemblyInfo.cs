e686971a0e6b9fd37fb94af52406d5ee25ee1c83
jrandlejr <randleja@hendrix.edu>
2026-03-10 19:26:54 -0500

    Added to the DB

--- MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
+++ MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e6bd9806efd72cf7ca893079db1f3c5e863946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b93bfac8f37d584b66810d99432faf99b50603b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
