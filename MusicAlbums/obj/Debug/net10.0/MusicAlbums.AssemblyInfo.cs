070dfe1bb6ab0c35a2adc5efb711c19830d041b7
jrandlejr <randleja@hendrix.edu>
2026-03-10 19:05:28 -0500

    Ran again to see if albums link would work. Succes

--- MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
+++ MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf60b75e02126a396c3ad8c1f9ba33aea9633bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cda59d99de3327b3bff5f0855051a063625903f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
